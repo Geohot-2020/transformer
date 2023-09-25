71db538e28f71c48193abfcb6000f8265846291d
zhengyc <1625098483@qq.com>
2023-09-25 14:28:11 +0800

    完成所有小组件，准备调包

--- abd_transformer.py
+++ abd_transformer.py
@@ -48,12 +48,12 @@ class LayerNorm(nn.Module):
 
 class SubLayerConnection(nn.Module):
     """
-    残差 + layernorm
+    残差 + layer_norm
     """
 
     def __init__(self, size, dropout=0.1):
         super(SubLayerConnection, self).__init__()
-        # layernorm
+        # layer_norm
         self.layer_norm = LayerNorm(size)
         # dropout
         self.dropout = nn.Dropout(p=dropout)
@@ -97,6 +97,7 @@ class MultiHeadAttention(nn.Module):
     """
     多头注意力机制
     """
+
     def __init__(self, head, d_model, dropout=0.1):
         """
         :param head: 头数， 默认为8
@@ -153,6 +154,7 @@ class PositionalEncoding(nn.Module):
     """
     位置编码
     """
+
     def __init__(self, dim, dropout, max_len=5000):
         """
         :param dim: 词向量维度，必须偶数位
@@ -192,3 +194,194 @@ class PositionalEncoding(nn.Module):
             emb = emb + self.pe[step]
         emb = self.drop_out(emb)
         return emb
+
+
+class PositionWiseFeedForward(nn.Module):
+    """
+    前馈神经网络FFN
+    w2(relu(w1(layer_norm(x))+b1)+b2
+    """
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionWiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.dropout_1 = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+        self.dropout_2 = nn.Dropout(dropout)
+
+    def forward(self, x):
+        inter = self.dropout_1(self.relu(self.w_1(self.layer_norm(x))))
+        output = self.dropout_2(self.w_2(inter))
+        return output
+
+
+class Generator(nn.Module):
+    """
+    Linear + softmax
+    """
+
+    def __init__(self, d_model, vocab_size):
+        super(Generator, self).__init__()
+        self.linear = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        return F.log_softmax(self.linear(x), dim=-1)
+
+
+def subsequent_mask(size):
+    """
+    掩码
+    :param size:
+    :return:
+    """
+    attn_size = (1, size, size)
+    mask = np.triu(np.ones(attn_size), k=1).astype('uint8')
+    return (torch.from_numpy(mask) == 0).cuda()
+
+
+def pad_mask(src, r2l_trg, trg, pad_idx):
+    if isinstance(src, tuple):
+        if len(src) == 4:
+            src_image_mask = (src[0][:, :, 0] != pad_idx).unsqueeze(1)
+            src_motion_mask = (src[1][:, :, 0] != pad_idx).unsqueeze(1)
+            src_object_mask = (src[2][:, :, 0] != pad_idx).unsqueeze(1)
+            src_rel_mask = (src[3][:, :, 0] != pad_idx).unsqueeze(1)
+            enc_src_mask = (src_image_mask, src_motion_mask, src_object_mask, src_rel_mask)
+            dec_src_mask_1 = src_image_mask & src_motion_mask
+            dec_src_mask_2 = src_image_mask & src_motion_mask & src_object_mask & src_rel_mask
+            dec_src_mask = (dec_src_mask_1, dec_src_mask_2)
+            src_mask = (enc_src_mask, dec_src_mask)
+        if len(src) == 3:
+            src_image_mask = (src[0][:, :, 0] != pad_idx).unsqueeze(1)
+            src_motion_mask = (src[1][:, :, 0] != pad_idx).unsqueeze(1)
+            src_object_mask = (src[2][:, :, 0] != pad_idx).unsqueeze(1)
+            enc_src_mask = (src_image_mask, src_motion_mask, src_object_mask)
+            dec_src_mask = src_image_mask & src_motion_mask
+            src_mask = (enc_src_mask, dec_src_mask)
+        if len(src) == 2:
+            src_image_mask = (src[0][:, :, 0] != pad_idx).unsqueeze(1)
+            src_motion_mask = (src[1][:, :, 0] != pad_idx).unsqueeze(1)
+            enc_src_mask = (src_image_mask, src_motion_mask)
+            dec_src_mask = src_image_mask & src_motion_mask
+            src_mask = (enc_src_mask, dec_src_mask)
+    else:
+        src_mask = (src[:, :, 0] != pad_idx).unsqueeze(1)
+    if trg is not None:
+        if isinstance(src_mask, tuple):
+            # 单句就行
+            trg_mask = (trg != pad_idx).unsqueeze(1) & subsequent_mask(trg.size(1)).type_as(src_image_mask.data)
+            r2l_pad_mask = (r2l_trg != pad_idx).unsqueeze(1).type_as(src_image_mask.data)
+            r2l_trg_mask = r2l_pad_mask & subsequent_mask(r2l_trg.size(1)).type_as(src_image_mask.data)
+            return src_mask, r2l_pad_mask, r2l_trg_mask, trg_mask
+        else:
+            trg_mask = (trg != pad_idx).unsqueeze(1) & subsequent_mask(trg.size(1)).type_as(src_mask.data)
+            r2l_pad_mask = (r2l_trg != pad_idx).unsqueeze(1).type_as(src_mask.data)
+            r2l_trg_mask = r2l_pad_mask & subsequent_mask(r2l_trg.size(1)).type_as(src_mask.data)
+            return src_mask, r2l_pad_mask, r2l_trg_mask, trg_mask  # src_mask[batch, 1, lens]  trg_mask[batch, 1, lens]
+
+    else:
+        return src_mask
+
+
+class EncoderLayer(nn.Module):
+    """
+    一层编码器
+    """
+
+    def __init__(self, size, attn, feed_forward, dropout=0.1):
+        """
+        :param size: 维度
+        :param attn: 注意力机制模块
+        :param feed_forward: 前馈神经网络
+        :param dropout: 避免过拟合
+        """
+        super(EncoderLayer, self).__init__()
+        self.attn = attn
+        self.feed_forward = feed_forward
+        # 残差连接
+        self.sublayer_connection = clones(SubLayerConnection(size, dropout), 2)
+
+    def forward(self, x, mask):
+        x = self.sublayer_connection[0](x, lambda x: self.attn(x, x, x, mask))
+        return self.sublayer_connection[1](x, self.feed_forward)
+
+
+class Encoder(nn.Module):
+    """
+    多层拼接
+    """
+
+    def __init__(self, n, encoder_layer):
+        """
+        :param n: 层数
+        :param encoder_layer: 单层，继承
+        """
+        super(Encoder, self).__init__()
+        self.encoder_layer = clones(encoder_layer, n)
+
+    def forward(self, x, src_mask):
+        for layer in self.encoder_layer:
+            x = layer(x, src_mask)
+        return x
+
+
+class DecoderLayer(nn.Module):
+    """
+    单层解码器
+    双向解码器
+    """
+
+    def __init__(self, size, attn, feed_forward, sublayer_num, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+        self.attn = attn
+        self.feed_forward = feed_forward
+        self.sublayer_connection = clones(SubLayerConnection(size, dropout), sublayer_num)
+
+    def forward(self, x, memory, src_mask, trg_mask, r2l_memory=None, r2l_trg_mask=None):
+        x = self.sublayer_connection[0](x, lambda x: self.attn(x, x, x, trg_mask))
+        x = self.sublayer_connection[1](x, lambda x: self.attn(x, memory, memory, src_mask))
+
+        if r2l_memory is not None:
+            x = self.sublayer_connection[-2](x, lambda x: self.attn(x, r2l_memory, r2l_memory, r2l_trg_mask))
+
+        return self.sublayer_connection[-1](x, self.feed_forward)
+
+
+class R2L_Decoder(nn.Module):
+    """
+    正向解码器
+    """
+
+    def __init__(self, n, decoder_layer):
+        super(R2L_Decoder, self).__init__()
+        self.decoder_layer = clones(decoder_layer, n)
+
+    def forward(self, x, memory, src_mask, r2l_trg_mask):
+        for layer in self.decoder_layer:
+            x = layer(x, memory, src_mask, r2l_trg_mask)
+        return x
+
+
+class L2R_Decoder(nn.Module):
+    """
+    反向解码器
+    """
+
+    def __init__(self, n, decoder_layer):
+        super(L2R_Decoder, self).__init__()
+        self.decoder_layer = clones(decoder_layer, n)
+
+    def forward(self, x, memory, src_mask, trg_mask, r2l_memory, r2l_trg_mask):
+        for layer in self.decoder_layer:
+            x = layer(x, memory, src_mask, trg_mask, r2l_memory, r2l_trg_mask)
+        return x
+
+
+"""
+=================上面不重要==============================
+=================下面调包才重要===========================
+"""
+# class ABDTransformer(nn.Module):
+#     def __init__(self, ):
